27ad61b348f2dd4f580f5bae240c20b95657d61c
Olia <ok.00100@gmail.com>
2020-02-14 03:23:18 +0300

    delete t-dist

--- t-dist/support/emuns/direction-type.enum.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var DirectionTypeEnum;
-(function (DirectionTypeEnum) {
-    DirectionTypeEnum["Sell"] = "sell";
-    DirectionTypeEnum["Buy"] = "buy";
-})(DirectionTypeEnum = exports.DirectionTypeEnum || (exports.DirectionTypeEnum = {}));
-//# sourceMappingURL=direction-type.enum.js.map
